84309c88b3efefa7a222220a6ffd1e79f1882d8f
razzellv <razzellv@nexumsuum.com>
2025-12-27 21:14:34 -0500

    Add authentication configuration for Cognito

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export const AUTH_CONFIG = {
+  domain: "https://us-east-2mkmqarq70.auth.us-east-2.amazoncognito.com",
+  clientId: "7vvu6kruod12nu1nkfonbfekre",
+  redirectUri: "https://portal.nexumsuum-facilityintelligence.com/auth/callback",
+  scope: "openid email profile",
+  responseType: "code"
+};
