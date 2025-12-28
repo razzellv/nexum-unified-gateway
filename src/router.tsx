832608a16f35c94dc235252d0c9ea0675181cf10
razzellv <razzellv@nexumsuum.com>
2025-12-28 08:04:59 -0500

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/login";
 import Index from "./pages/Index";
 import AuthCallback from "./pages/AuthCallback";
-import ProtectedRoute from "./auth/ProtectedRoute";
+import ProtectedRoute from "./auth/protectedroute";
 
 export const router = createBrowserRouter([
   {
