6f47a9303ad3134bfce4db930bb5d4703ce65f9b
razzellv <razzellv@nexumsuum.com>
2025-12-28 08:25:16 -0500

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,25 +1,32 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import App from "./App";
+
 import Login from "./pages/login";
 import Index from "./pages/Index";
-import AuthCallback from "./pages/AuthCallback";
+
 import ProtectedRoute from "./auth/protectedroute";
 
 export const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/auth/callback",
-    element: <AuthCallback />,
-  },
-  {
-    path: "/",
-    element: <ProtectedRoute />,
+    element: <App />,
     children: [
       {
-        index: true,
-        element: <Index />,
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/",
+        element: <ProtectedRoute />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+        ],
+      },
+      {
+        path: "*",
+        element: <Navigate to="/login" replace />,
       },
     ],
   },
