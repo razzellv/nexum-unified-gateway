202b700e06500226247d48a2cad51973e35914d5
razzellv <razzellv@nexumsuum.com>
2025-12-27 21:34:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,18 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Courses from "./pages/Courses";
-import Equipment from "./pages/Equipment";
-import Compliance from "./pages/Compliance";
-import Virtuous from "./pages/Virtuous";
-import NotFound from "./pages/NotFound";
+import { Outlet } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/equipment" element={<Equipment />} />
-          <Route path="/compliance" element={<Compliance />} />
-          <Route path="/virtuous" element={<Virtuous />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <Outlet />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
