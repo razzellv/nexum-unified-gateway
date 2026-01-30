b6c292f8e37411778ec9674b12ad4986463f69a0
razzellv <razzellv@nexumsuum.com>
2026-01-30 18:29:16 -0500

    Refactor EquipmentIntelligence layout and add new sections
    Refactor EquipmentIntelligence component to include MainLayout and ParticleBackground. Added RecentEquipment and InventorySection components.

--- src/pages/EquipmentIntelligence.tsx
+++ src/pages/EquipmentIntelligence.tsx
@@ -1,18 +1,35 @@
+import { MainLayout } from '@/components/MainLayout';
+import { ParticleBackground } from '@/components/ParticleBackground';
+import { NexumBranding } from '@/components/NexumBranding';
 import HeroSection from "@/components/equipment-intelligence/HeroSection";
 import UploadSection from "@/components/equipment-intelligence/UploadSection";
 import AnalysisResults from "@/components/equipment-intelligence/AnalysisResults";
 import EquipmentLibrary from "@/components/equipment-intelligence/EquipmentLibrary";
+import RecentEquipment from "@/components/equipment-intelligence/RecentEquipment"; // NEW
+import InventorySection from "@/components/equipment-intelligence/InventorySection"; // NEW
 import { EquipmentProvider } from "@/contexts/EquipmentContext";
 
 export default function EquipmentIntelligence() {
   return (
-    <EquipmentProvider>
-      <div className="min-h-screen bg-background">
-        <HeroSection />
-        <UploadSection />
-        <AnalysisResults />
-        <EquipmentLibrary />
-      </div>
-    </EquipmentProvider>
+    <MainLayout>
+      <ParticleBackground />
+      <NexumBranding />
+      
+      <EquipmentProvider>
+        <div className="space-y-6">
+          <HeroSection />
+          <UploadSection />
+          <AnalysisResults />
+          
+          {/* NEW: Recent Equipment (Last 7 Days) */}
+          <RecentEquipment />
+          
+          <EquipmentLibrary />
+          
+          {/* NEW: Inventory Section */}
+          <InventorySection />
+        </div>
+      </EquipmentProvider>
+    </MainLayout>
   );
 }
