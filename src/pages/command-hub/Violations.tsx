84dc939d7bd174da1f6b1cdef41460cf54623c37
razzellv <razzellv@nexumsuum.com>
2026-02-15 21:06:31 -0500

    Fix: Add backticks to Violations (perl)

--- src/pages/command-hub/Violations.tsx
+++ src/pages/command-hub/Violations.tsx
@@ -32,7 +32,7 @@ export default function Violations() {
 
     try {
 
-      const response = await fetch${API_BASE_URL}/violations?facilityId=${user.facilityId}, {
+      const response = await fetch/violations?facilityId=, {
 
         headers: { 'Authorization': Bearer ${localStorage.getItem('nexum_access_token')} }
 
